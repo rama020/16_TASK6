4be17df31a76139770770ba0743f5349bebed4e5
rama020 <118764128+rama020@users.noreply.github.com>
2022-12-09 20:30:12 +0530

    Add files via upload

---
+++ TASK6.py
@@ -0,0 +1,21 @@
+'''
+DATE:09TH NOV 2022
+DAY: FRIDAY
+TOPIC: TASK
+AUTHOR: RAMA BHARGAVI
+'''
+#not to display continue
+n=0
+while(n<10):
+    n+=1
+    if n==6:
+      print(n) #1 to 10
+#retrieve only negatives
+n=[5,10,15,20,-4,-3,-1,0,8]
+for i in n:
+   if(i<0):
+      print(i) #-4 -3 -1
+
+
+
+    
